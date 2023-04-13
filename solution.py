12f10c34faf6fa76998a36b5369ef7efa2b1a4be
Murzik2020ai <61930598+Murzik2020ai@users.noreply.github.com>
2023-04-13 12:10:17 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,8 +3,8 @@ import numpy as np
 
 
 chat_id = 1226526788
+from scipy.stats import ttest_ind
 
-from scipy.stats import wilcoxon
 def solution(x: np.array, y:np.array) -> bool:
-    pvalue = wilcoxon(x,y).pvalue
-    return True if pvalue < 0.03 else False
+  pvalue = ttest_ind(x,y,equal_var=False).pvalue
+  return True if pvalue < 0.03 else False
